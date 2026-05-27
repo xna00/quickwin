af6e59044fd7399fdef0f7b9ceb30d008736562f
xna00 <xna00@qq.com>
2026-05-26 23:21:44 -0700

    feat: export component Props types from index.ts

--- lib/preact/components/index.ts
+++ lib/preact/components/index.ts
@@ -1,5 +1,14 @@
 export { Button } from './Button.js'
+export type { ButtonProps } from './Button.js'
+
 export { EditBox } from './EditBox.js'
+export type { EditBoxProps } from './EditBox.js'
+
 export { ListBox } from './ListBox.js'
+export type { ListBoxProps } from './ListBox.js'
+
 export { Tab } from './Tab.js'
+export type { TabProps } from './Tab.js'
+
 export { ListView } from './ListView.js'
+export type { ListViewProps } from './ListView.js'
