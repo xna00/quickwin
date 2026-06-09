371ace29ecf7eebc69dd784a0e9d706b2c160e29
xna00 <xna00@qq.com>
2026-06-08 22:34:38 -0700

    reconciler: enable supportsMicrotasks for better React scheduling

--- lib/react-qw/reconciler.ts
+++ lib/react-qw/reconciler.ts
@@ -165,7 +165,7 @@ const hostConfig: QuickWinHostConfig = {
   supportsPersistence: false,
   supportsHydration: false,
   isPrimaryRenderer: true,
-  supportsMicrotasks: false,
+  supportsMicrotasks: true,
 
   setCurrentUpdatePriority(p: number) { currentUpdatePriority = p },
   getCurrentUpdatePriority() { return currentUpdatePriority },
